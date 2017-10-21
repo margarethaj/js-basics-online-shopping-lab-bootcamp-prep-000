94630acd80aed59a1e6f336f3672b4e034c8c2f2
Margaretha Juan <tulip_holy@yahoo.com>
2017-10-21 18:19:23 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -83,7 +83,8 @@ function removeFromCart(item) {
 function placeOrder(cardNumber) {
   // write your code here
   if(cardNumber == null){
-    console.log("doesn't place the order if a credit card number is not provided")
+    console.log("Sorry, we don't have a credit card on file for you.")
+    return "doesn't place the order if a credit card number is not provided"
   }
   else{
     
